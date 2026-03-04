b24a5382a0b6314690d3ef8a0549cbe4b9b752af
jorge <jorge@marksearch.online>
2026-03-04 15:25:45 +0100

    info from /projecst page  moved to home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,32 +10,22 @@ export default function Home() {
   const titleRef = useRef<HTMLDivElement>(null);
   const pitchRef = useRef<HTMLDivElement>(null);
   const collabRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
   const officeRef = useRef<HTMLDivElement>(null);
 
   // Only english and german
   const [lang, setLang] = useState<'en' | 'de'>('en');
-
-  // Multiple languages
-  // const languages: Array<'en'|'de'|'en'|'fr'|'en'|'it'|'en'|'rm'> = ['en','de','en','fr','en','it','en','rm'];
-  // const [langIndex, setLangIndex] = useState(0);
-  // const lang = languages[langIndex];
 
   const isVisible = usePageVisibility();
 
   const translations = {
     title: {
       en: 'Paraclet',
-      de: 'Paraclet',
-      fr: 'Paraclet',
-      it: 'Paraclet',
-      rm: 'Paraclet'
+      de: 'Paraclet'
     },
     pitch: {
       en: 'We develop AI-native solutions for architects and designers.',
-      de: 'Wir entwickeln KI-native Lösungen für Architekten und Designer.',
-      fr: 'Nous développons des solutions natives en IA pour les architectes et les designers.',
-      it: 'Sviluppiamo soluzioni native per l’IA destinate ad architetti e designer.',
-      rm: 'Nus sviluppain soluziuns nativas cun IA per architects ed indeschinaders.'
+      de: 'Wir entwickeln KI-native Lösungen für Architekten und Designer.'
     },
     collab: {
       en: `Selected clients:<br/>
@@ -48,22 +38,28 @@ export default function Home() {
         <a href="https://de-vylder.arch.ethz.ch/" target="_blank" rel="noopener noreferrer">a studio – jan de vylder</a><br/>
         <a href="https://meteora.ch/" target="_blank" rel="noopener noreferrer">Meteora ETH</a><br/>
         <a href="https://0more.net/" target="_blank" rel="noopener noreferrer">Studio 0More</a>`,
-      fr: 'Clients sélectionnés:<br/>pool Architekten<br/>a studio – jan de vylder<br/>Meteora ETH<br/>Studio 0More',
-      it: 'Clienti selezionati:<br/>pool Architekten<br/>a studio – jan de vylder<br/>Meteora ETH<br/>Studio 0More',
-      rm: 'Clients tschernids:<br/>pool Architekten<br/>a studio – jan de vylder<br/>Meteora ETH<br/>Studio 0More'
+    },
+    projects: {
+      en: `Selected projects:<br/>
+        <a href="https://marksearch.online/" target="_blank" rel="noopener noreferrer">Mark</a><br/>
+        <a href="https://jdv.marksearch.online/" target="_blank" rel="noopener noreferrer">round a round</a><br/>
+        <a href="https://panoramasofcinema.ch/" target="_blank" rel="noopener noreferrer">Panoramas of Cinema</a><br/>
+        <a href="https://search.0more.net/" target="_blank" rel="noopener noreferrer">Search 0More</a>`,
+      de: `Ausgewählte Projekte:<br/>
+        <a href="https://marksearch.online/" target="_blank" rel="noopener noreferrer">Mark</a><br/>
+        <a href="https://jdv.marksearch.online/" target="_blank" rel="noopener noreferrer">round a round</a><br/>
+        <a href="https://panoramasofcinema.ch/" target="_blank" rel="noopener noreferrer">Panoramas of Cinema</a><br/>
+        <a href="https://search.0more.net/" target="_blank" rel="noopener noreferrer">Search 0More</a>`,
     },
     office: {
       en: `Paraclet<br/>
         Geroldstrasse 31b<br/>
-        CH-8005 Zurich<br><br/>
+        CH-8005 Zürich<br><br/>
         <a href="mailto:office@paraclet.io">office@paraclet.io</a>`,
       de: `Paraclet<br/>
         Geroldstrasse 31b<br/>
         CH-8005 Zürich<br><br/>
         <a href="mailto:office@paraclet.io">office@paraclet.io</a>`,
-      fr: 'Paraclet<br>Geroldstrasse 31b<br>CH-8005 Zurich<br><br>office@paraclet.io',
-      it: 'Paraclet<br>Geroldstrasse 31b<br>CH-8005 Zurigo<br><br>office@paraclet.io',
-      rm: 'Paraclet<br>Geroldstrasse 31b<br>CH-8005 Turitg<br><br>office@paraclet.io'
     }
   };
 
@@ -88,19 +84,9 @@ export default function Home() {
     return () => clearInterval(id);
   }, [isVisible]);
 
-  // Toggle multiple languages
-  // useEffect(() => {
-  //   if (!isVisible) return;
-
-  //   const id = setInterval(() => {
-  //     setLangIndex((prev) => (prev + 1) % languages.length);
-  //   }, 5000);
-  //   return () => clearInterval(id);
-  // }, [isVisible]);  
-
-  // Scramble all three text areas on language change
+  // Scramble all text areas on language change
   useEffect(() => {
-    [titleRef, pitchRef, collabRef, officeRef].forEach(ref => {
+    [titleRef, pitchRef, collabRef, projectsRef].forEach(ref => {
       if (!ref.current) return;
       const opts = ref === titleRef ? scrambleOptsSlow : scrambleOpts;
       const scr = new ScrambleText(ref.current, {
@@ -118,18 +104,17 @@ export default function Home() {
         {translations.title[lang]}
       </div>
 
-      <div className={styles.pitch}>
-        <a href="https://www.paraclet.io/projects">
-          <span ref={pitchRef}>{translations.pitch[lang]}</span>
-        </a>
-      </div>
+      <div className={styles.pitch} ref={pitchRef}>{translations.pitch[lang]}</div>
       
-      <div className={styles.footer}>
+      <div className={styles.projectsWrap}>
         <div className={styles.collaborators} ref={collabRef} dangerouslySetInnerHTML={{ __html: translations.collab[lang] }}></div>
-        <div className={styles.office} ref={officeRef} dangerouslySetInnerHTML={{ __html: translations.office[lang] }}></div>
+        <div className={styles.collaborators} ref={projectsRef} dangerouslySetInnerHTML={{ __html: translations.projects[lang] }}></div>
       </div>
 
-      <div className={styles.cr}>© 2026 Paraclet</div>
+      <div className={styles.footer}>
+        <div className={styles.office} ref={officeRef} dangerouslySetInnerHTML={{ __html: translations.office[lang] }}></div>
+        <div className={styles.cr}>© 2026 Paraclet</div>
+      </div>
     </div>
   )
 }
